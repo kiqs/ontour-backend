5383d37159f72839bfd2fadfc100edd47d9bcab3
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-06-12 17:15:55 +0300

    clean code

--- server/index.js
+++ server/index.js
@@ -16,32 +16,3 @@ app.use(routes());
 app.use(ctx => ctx.status = 404);
 
 export default app;
-
-// const bodyParser = require('koa-bodyparser');
-// const session = require('koa-session')
-// const http = require('http');
-// const config = require('./config/config');
-// const passport = require('./config/passport');
-// const err = require('./error');
-// const { routes, allowedMethods } = require('./routes/routes');
-// const securedRouter = require('./routes/secure-routes');
-
-// app.use(bodyParser());
-// app.use(session({}, app));
-//
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(err);
-// app.use(routes());
-// app.use(allowedMethods());
-// app.use(securedRouter.middleware());
-//
-// const server = http.createServer(app.callback()).listen(config.server.port, () => {
-//     console.log('%s listening at port %d', config.app.name, config.server.port);
-// });
-
-// module.exports = {
-//     closeServer() {
-//         server.close();
-//     }
-// };
