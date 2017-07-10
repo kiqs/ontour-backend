050b60db1f97dd0d005250803756f75b8a578952
Kirill Stepkin <kirillstyopkin@gmail.com>
2017-07-10 23:34:16 +0300

    update upcoming events request

---
+++ server/constants/api.js
@@ -0,0 +1,2 @@
+export const SK_DOMAIN = 'https://api.songkick.com/api/3.0';
+export const SK_KEY = 'iOAsnWYdLjjhNvvM';
--- server/routes/api/events.js
+++ server/routes/api/events.js
@@ -1,11 +1,26 @@
 'use strict';
 
+import superagent from 'superagent';
 import Event from '../../models/Event';
 import router from '../router';
+import * as constants from '../../constants/api';
 
 router.get('/events', async(ctx, next) => {
   try {
     ctx.body = await Event.find();
+  }
+  catch (err) {
+    ctx.status = 400;
+    ctx.body = err;
+  }
+});
+
+router.post('/upcoming-events', async(ctx, next) => {
+  try {
+    let url = `${ctx.request.body.link}?apikey=${constants.SK_KEY}&page=${ctx.request.body.page}&per_page=${ctx.request.body.per_page}`;
+    let res = await superagent.get(`${url}`).withCredentials();
+    let results = JSON.parse(res.text);
+    ctx.body = results.resultsPage;
   }
   catch (err) {
     ctx.status = 400;
